8b3af362c45edba9991735420c84c23f50a2c655
rawadabu <rawadabusaleh0@gmail.com>
2023-11-05 12:19:17 +0200

    Loader

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import Header from "./Header";
 import CartOverView from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  //  IDLE OR LOADING console.log(navigation);
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
