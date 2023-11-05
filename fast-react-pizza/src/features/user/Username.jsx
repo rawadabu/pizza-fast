31b29214aa718f84dbef3b158932dad71d7299bb
rawadabu <rawadabusaleh0@gmail.com>
2023-11-05 17:07:15 +0200

    Tailwind: Flexbox 🎨

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Rawad</div>;
+  return <div className="hidden text-sm font-semibold md:block">Rawad</div>;
 }
 
 export default Username;
