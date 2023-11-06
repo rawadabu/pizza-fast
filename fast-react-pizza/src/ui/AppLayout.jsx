80c3080d64c585d34fd86cf652a9ee45390b3b29
rawadabu <rawadabusaleh0@gmail.com>
2023-11-06 10:48:58 +0200

    Tailwind: using CSS grid.

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,20 +1,22 @@
 /* eslint-disable no-unused-vars */
-import Header from "./Header";
-import CartOverView from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import Header from './Header';
+import CartOverView from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 
 function AppLayout() {
   const navigation = useNavigation();
   //  IDLE OR LOADING console.log(navigation);
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl ">
+          <Outlet />
+        </main>
+      </div>
       <CartOverView />
     </div>
   );
