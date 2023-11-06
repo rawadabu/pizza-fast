e44f30b5de17b4499e1aede5b70da60d148999b7
rawadabu <rawadabusaleh0@gmail.com>
2023-11-06 12:37:21 +0200

    Tailwind: Absolute positioning, z-index and more 🎨

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -10,6 +10,7 @@ function AppLayout() {
   const isLoading = navigation.state === 'loading';
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {/* {true && <Loader />} */}
       {isLoading && <Loader />}
       <Header />
       <div className="overflow-scroll">
