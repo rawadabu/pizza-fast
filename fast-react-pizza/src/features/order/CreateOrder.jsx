ef4d5e945d7972f396f63257c63fa101ceecacc2
rawadabu <rawadabusaleh0@gmail.com>
2023-11-07 11:15:49 +0200

    Redux: modeling username state, reading and updating ✅

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,6 +3,7 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiResturant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 // eslint-disable-next-line no-unused-vars
@@ -42,6 +43,8 @@ function CreateOrder() {
 
   const formErrors = useActionData(); // We get this when is not submittion made, error or somethings
 
+  const username = useSelector((state) => state.user.username); // READ DATA FROM THE REDUX STORE
+
   const cart = fakeCart;
 
   return (
@@ -51,7 +54,13 @@ function CreateOrder() {
       <Form method="POST" action="/order/new">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
---
+++ fast-react-pizza/src/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './features/user/UserSlice';
+
+const store = configureStore({ reducer: { user: userReducer } });
+
+export default store;
